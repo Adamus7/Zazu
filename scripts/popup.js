3b45acb4159420e8c8607a885ea7bcdd0ade754d
Adamus <adamus_7@outlook.com>
2018-07-31 16:45:54 +0800

    remove the useless comments

--- scripts/popup.js
+++ scripts/popup.js
@@ -3,11 +3,8 @@ function onPageShow() {
   browser.tabs.query(
     { currentWindow: true, active: true },
     function (tabs) {
-      var url = tabs[0].url;
       var title = tabs[0].title;
-      // browser.tabs.create({ url: "http://www.microsofttranslator.com/bv.aspx?from=&to=en&a=" + url, selected: true })
-      // alert(title)
-      // alert(url)
+      var url = tabs[0].url;
       $('input[id="fav-name"]').val(title);
       $('input[id="fav-url"]').val(url);
     });
